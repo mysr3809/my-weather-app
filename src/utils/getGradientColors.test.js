b53c58a40b0cf76b5d168da2eeaa1432ba44351c
mysr3809 <myasar0938@gmail.com>
2024-03-30 13:12:26 +0100

    update the test value

--- src/utils/getGradientColors.test.js
+++ src/utils/getGradientColors.test.js
@@ -4,19 +4,19 @@ import getGradientColors from "./setGradient";
 describe("getGradientColors", () => {
   test("returns correct gradient for temperature less than -30", () => {
     expect(getGradientColors(-31)).toEqual(
-      "linear-gradient(to bottom right, #a1c4fd, #173070)"
+      "linear-gradient(to bottom right, #87b5c55e, #173070)"
     );
   });
 
   test("returns correct gradient for temperature between -30 and -20", () => {
     expect(getGradientColors(-25)).toEqual(
-      "linear-gradient(to bottom right, #a1c4fd, #347cbb)"
+      "linear-gradient(to bottom right, #87b5c55e, #347cbb)"
     );
   });
 
   test("returns correct gradient for temperature between -20 and -10", () => {
     expect(getGradientColors(-15)).toEqual(
-      "linear-gradient(to bottom right, #a1c4fd, #3a8bc6)"
+      "linear-gradient(to bottom right, #87b5c55e, #3a8bc6)"
     );
   });
 });
